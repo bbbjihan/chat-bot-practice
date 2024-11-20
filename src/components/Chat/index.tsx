bbdf7a87a1b335b622d9211974c521c53bf90064
bbbjihan <74421125+bbbjihan@users.noreply.github.com>
2024-11-20 16:40:03 +0900

    design: 최대 가로길이 설정, 중앙정렬

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,3 +1,4 @@
+import { Box } from "@mui/material";
 import ChatHistory from "./ChatHistory";
 import ChatInput from "./ChatInput";
 import useChat from "./useChat";
@@ -5,10 +6,12 @@ import useChat from "./useChat";
 const Chat = () => {
   const { chatInputProps, chatHistoryProps } = useChat();
   return (
-    <>
-      <ChatHistory {...chatHistoryProps} />
-      <ChatInput {...chatInputProps} />
-    </>
+    <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
+      <Box sx={{ width: "100%", maxWidth: 712 }}>
+        <ChatHistory {...chatHistoryProps} />
+        <ChatInput {...chatInputProps} />
+      </Box>
+    </Box>
   );
 };
 
